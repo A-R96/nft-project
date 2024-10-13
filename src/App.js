3195643339923fb8a0ca444d249dc6210f60a9bd
A-R96 <andrewrush1@hotmail.com>
2024-10-13 10:48:09 -0400

    New page design updated styles

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import WalletBalance from './components/WalletBalance';
 import ImageBanner from './components/ImageBanner';
 import { rdt } from './radixConfig';
 import './App.css';
+import { FaInstagram, FaXTwitter } from 'react-icons/fa6';
+import { FaTelegram } from 'react-icons/fa';
 
 function App() {
   const [walletData, setWalletData] = useState(null);
@@ -80,9 +82,33 @@ function App() {
 
   return (
     <div className="App">
-      <Header onConnectClick={handleConnectClick} />
-      <main>
-        <div className="content-wrapper">
+      <div className="content-wrapper">
+        <nav className="nav-menu">
+          <a href="#project-description">Project Description</a>
+          <a href="#tokenomics">Tokenomics</a>
+          <a href="#roadmap">Roadmap</a>
+          <a href="#battle">Battle</a>
+          <a href="#mint">Mint</a>
+        </nav>
+        <img src={process.env.PUBLIC_URL + '/images/title.png'} alt="CAPYCLUB" className="title-overlay" />
+        <div className="social-buttons">
+          <a href="#" className="social-button twitter">
+            <FaXTwitter />
+          </a>
+          <a href="#" className="social-button telegram">
+            <FaTelegram />
+          </a>
+          <a href="#" className="social-button instagram">
+            <FaInstagram />
+          </a>
+        </div>
+        {/* Temporary placeholder div */}
+        <div style={{ height: '100%', width: '100%' }}></div>
+
+        {/* Commented out components */}
+        {/*
+        <Header onConnectClick={handleConnectClick} />
+        <main>
           <ProjectDescription />
           <div className="tables-container">
             <TraitsTable />
@@ -95,8 +121,10 @@ function App() {
             rdt={rdt}
           />
           <ImageBanner />
-        </div>
-      </main>
+          <img src="/images/title.jpg" alt="Title" className="title-overlay" />
+        </main>
+        */}
+      </div>
     </div>
   );
 }
