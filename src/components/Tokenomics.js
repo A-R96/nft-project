0d3e9c3d947bcdacaf01229d72410bbf04b4eb7b
A-R96 <andrewrush1@hotmail.com>
2024-10-20 17:32:51 -0400

    fix tokenomics background image cutting off and add styles to sale section

--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -4,10 +4,7 @@ import './Tokenomics.css';
 function Tokenomics() {
   return (
     <section className="page-content tokenomics-content">
-      <div className="tokenomics-container">
-        <h2>Tokenomics</h2>
-        {/* Add your tokenomics content here */}
-      </div>
+    
     </section>
   );
 }
